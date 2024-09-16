4f5ddf45d308ed6ccf58c7d34c4b595ba41927f2
AndersonSN21 <anderson.sabino.barboza.silva@ccc.ufcg.edu.br>
2024-09-16 20:22:35 -0300

    entregando o lab

--- src/main/java/InsereOrdenadoFilaPrioridade.java
+++ src/main/java/InsereOrdenadoFilaPrioridade.java
@@ -17,12 +17,36 @@ public class InsereOrdenadoFilaPrioridade implements FilaPrioridade {
 			this.head++;
 			this.last++;
 			fila[last] = newPair;
-		} else if (this.head == this.last){
-		}
 		
-		int j = this.head;
-		while (j < this.last && fila[j] > fila) {
+		} else if (this.head == this.last){
+			if (this.fila[this.last].getPrioridade() < prioridade) {
+				Pair aux = this.fila[this.last];
+				this.fila[this.last] = newPair;
+				this.last = (this.last + 1) % this.fila.length;
+				this.fila[this.last] = aux; 
+			
+			} else {
+				this.last = (this.last + 1) % this.fila.length;
+				this.fila[this.last] = newPair;
+			}
+		
+		} else {
+			this.last = (this.last + 1) % this.fila.length;
+        	this.fila[this.last] = newPair;
 
+			int i = this.last;
+            while (i != this.head) {
+                int prev = (i - 1 + this.fila.length) % this.fila.length;
+                
+				if (this.fila[i].getPrioridade() > this.fila[prev].getPrioridade()) {
+                    Pair aux = this.fila[i];
+                    this.fila[i] = this.fila[prev];
+                    this.fila[prev] = aux;
+                    i = prev;
+				} else {
+					break;
+				}
+			}
 		}
 	}
 
@@ -30,7 +54,13 @@ public class InsereOrdenadoFilaPrioridade implements FilaPrioridade {
 	// remover e retornar o primeiro elemento do array, que é o de maior prioridade. lembrar manipular head e tail
 	// para ser uma fila circular. assim a remoção fica O(1)
 	public String removeNext() {
-		return "";
+		Pair aux = fila[this.head];
+        if (this.head == this.last) {
+            this.head = -1;
+            this.last = -1;
+        } else {
+            this.head = (this.head + 1) % this.fila.length;
+        }
+        return aux.getElemento();
 	}
-
 }
